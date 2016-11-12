6358db5578b3f8a25ce8cdf8d25eab31dafe4ac9
JasonGUTU <hellojasongt@gmail.com>
2016-11-12 20:34:33 +0800

    setup tools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'NAA',
+    version = '0.0.1',
+    keywords = ('Global Optimization', 'Evolutionary computation', 'Natural Aggregation Algorithm'),
+    description = 'Natural Aggregation Algorithm: A New Powerful Global Optimizer over Continuous Spaces',
+    license = 'MIT License',
+    install_requires = ['numpy'],
+
+    author = 'Jason GT',
+    author_email = 'hellojasongt@gmail.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
